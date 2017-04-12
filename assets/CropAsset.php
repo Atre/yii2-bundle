92903a125161852a58d594caf5710f6706521651
dezmont765 <dezmont765@gmail.com>
2017-04-12 19:44:42 +0300

    Crop asset

---
+++ assets/CropAsset.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+namespace dezmont765\yii2bundle\assets;
+
+use dezmont765\yii2bundle\views\MainView;
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class CropAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'libs/cropper-ui/jquery.Jcrop.min.css',
+    ];
+    public $js = [
+        'libs/cropper-ui/jquery.Jcrop.min.js',
+        'libs/cropper-ui/jaml.js',
+        'libs/cropper-ui/Crop.js',
+    ];
+    public $jsOptions = [
+        'position' => MainView::POS_END,
+    ];
+    public $depends = [
+        'backend\assets\AppAsset'
+    ];
+}
